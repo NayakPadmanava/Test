bbe97279e54ec20b06bbd9ba17df786d398e3109
Padmanava Nayak <nayak.padma84@gmail.com>
2018-12-12 09:31:15 +0530

    Added Code for IFElse

---
+++ IFElse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElseDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the Color");
+            string color = Console.ReadLine();
+            bool flag = true;
+            int i = 5;
+            if (color == "Red")
+            {
+                Console.WriteLine("Color Is Red");
+            }
+            else if (color == "Blue")
+            {
+                Console.WriteLine("color is Blue");
+            }
+            else
+            {
+                Console.WriteLine("Color is not matching");
+            }
+            Console.ReadLine();
+        }
+    }
+}
