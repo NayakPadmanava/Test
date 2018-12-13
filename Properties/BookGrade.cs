82e1abedcdb5a561de8b0fafa181c727c235a2c4
Padmanava Nayak <nayak.padma84@gmail.com>
2018-12-13 10:40:04 +0530

    added for properties code

---
+++ Properties/BookGrade.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesDemo
+{
+    class BookGrade
+    {
+        private int score;
+        private int percentage;
+        private char grade;
+        
+
+        public string Name { get; set; }
+        public char Grade
+        {
+            get
+            {
+                if (score == 50)
+                {
+
+                    return grade = 'A';
+                }
+                else
+                {
+                    return grade = 'B';
+                }
+            }
+            // set { grade = value; }
+        }
+
+        public int Marks
+        {
+            set { score=value; }
+            get {
+                if(score > 50)
+                { return score; }
+                else
+                {
+                    return score=0;
+                }
+                 }
+        }
+
+        public int Percentage
+        {
+            get { return percentage; }
+            set {  percentage = value; }
+        }
+
+
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookGrade ob = new BookGrade();
+            ob.Marks = 40;
+            ob.Percentage = 40;
+            ob.Name = "Naveen";
+            //ob.Grade = 60;
+
+            Console.WriteLine(ob.Marks);
+            Console.WriteLine(ob.Percentage);
+            Console.WriteLine(ob.Grade);
+            Console.WriteLine(ob.Name);
+            Console.ReadLine();
+        }
+    }
+}
