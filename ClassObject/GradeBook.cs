6d7a15bd319319bc9c3938b934970ec23436471b
Padmanava Nayak <nayak.padma84@gmail.com>
2018-12-15 12:08:17 +0530

    Added for ClassObject concept

---
+++ ClassObject/GradeBook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoClass
+{
+    class GradeBook
+    {
+        int[] gradeObj;
+        //public GradeBook()
+        //{
+        //    //Constructor
+        //    gradeObj = new int[5];
+        //    Console.WriteLine("Constructor is getting call");
+        //}
+
+        public GradeBook(int num)
+        {
+            gradeObj = new int[5];
+            Console.WriteLine(num);
+        }
+        public GradeBook( GradeBook obj)
+        {
+
+            Console.WriteLine("Copy constructor");
+        }
+        public int AddGrade(int grade)
+        {
+            gradeObj[0] = grade;
+            return grade;
+        }
+
+    }
+}
---
+++ ClassObject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook ob = new GradeBook(67);
+            GradeBook obj = new GradeBook(ob);
+            ob.AddGrade(30);
+            Console.WriteLine(ob.AddGrade(30));
+            Console.ReadLine();
+        }
+    }
+}
