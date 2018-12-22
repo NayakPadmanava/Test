a40f64eabcec8e4af868a8b5877b36282b4f3691
Padmanava Nayak <nayak.padma84@gmail.com>
2018-12-22 12:14:23 +0530

    added for assembly

---
+++ Assembly/Project/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoAssembly;
+
+namespace UsingAssemblyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PA ob = new PA();
+            string str = ob.GetMethod();
+
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
