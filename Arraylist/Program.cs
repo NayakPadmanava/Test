fb8de1fafbcdec1bacfaa0e0b3a61e538f71b76e
Padmanava Nayak <nayak.padma84@gmail.com>
2019-01-03 18:29:01 +0530

    Added file for Arraylist and Hashtable

---
+++ Arraylist/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList obArrayList = new ArrayList();
+            obArrayList.Add(50);
+            obArrayList.Add("Naveen:Anupam");
+            obArrayList.Add("Arjun");
+            obArrayList.Add('M');
+            obArrayList.Add("SDLC");
+            Console.WriteLine(obArrayList.Count);
+            
+            obArrayList.Capacity = 10;
+            
+            foreach( var ar in obArrayList)
+            {
+                Console.WriteLine(ar);
+            }
+            obArrayList.Remove("Naveen:Anupam");
+             obArrayList.Insert(0, 100);
+            foreach (var ar in obArrayList)
+            {
+                Console.WriteLine(ar);
+
+            }
+
+             obArrayList.Insert(0, 200);
+            obArrayList.RemoveAt(1);
+            foreach (var ar in obArrayList)
+            {
+                Console.WriteLine(ar);
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Hashtable/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoHashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable obList = new Hashtable();
+            obList.Add("Name", "Naveen");
+            obList.Add("age", 100);
+            obList.Add("sal", 10000);
+            Console.WriteLine(obList.Count);
+            //foreach(DictionaryEntry ar in obList)
+            //{
+            //    Console.WriteLine("Key:{0}, value:{1}", ar.Key,ar.Value);
+            //}
+            //obList.Clear();
+            foreach(var key in obList.Keys)
+            {
+                Console.WriteLine(key.ToString());
+            }
+            foreach (var key in obList.Values)
+            {
+                Console.WriteLine(key.ToString());
+            }
+            //obList.Remove("Name");
+            //foreach (var key in obList.Keys)
+            //{
+            //    Console.WriteLine(key.ToString());
+            //}
+
+            //Console.WriteLine(obList["age"].ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
