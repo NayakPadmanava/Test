81257ed7bf9bbdce6c9c69d38b655f83d7aa4f77
Padmanava Nayak <nayak.padma84@gmail.com>
2019-01-05 12:38:34 +0530

    Added all the values for collection

---
+++ DemoCollection/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace DemoQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue ob = new Queue();
+            ob.Enqueue("Ashok");
+            ob.Enqueue("Anupam");
+                ob.Enqueue("Nawab");
+            ob.Dequeue();
+            ob.Dequeue();
+            foreach (var item in ob)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DemoCollection/ProgramStack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace DemoStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack ob = new Stack();
+            ob.Push(10);
+            ob.Push(20);
+           // ob.Push(null);
+            
+            //Console.WriteLine(ob.Pop());
+            Console.WriteLine(ob.Peek()); //20
+            Console.WriteLine(ob.Pop()); //20
+            foreach (var dt in ob)
+            {
+                Console.WriteLine(dt); //10
+            }
+            Console.ReadLine();
+        }
+    }
+}
