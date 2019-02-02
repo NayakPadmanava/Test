74003b194519244ea6214c55963a50de28c46d23
Padmanava Nayak <nayak.padma84@gmail.com>
2019-02-02 22:19:37 +0530

    MVC FirstDemo

---
+++ MVC/New folder/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using DemoModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoModel.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Employee()
+        {
+            Employee obemp = new Employee();
+            List<Employee> oblist = obemp.GetEmployees();
+            ViewBag.EmpDtl = oblist;
+            
+
+            return View();
+        }
+        public ActionResult EmployeeViewData()
+        {
+            Employee obemp = new Employee();
+            List<Employee> oblist = obemp.GetEmployees();
+            ViewData["Empdtl1"] = oblist;
+
+            return View();
+        }
+
+
+    }
+}
---
+++ MVC/New folder/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoModel.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public double EmpSal { get; set; }
+
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> oblist = new List<Employee> { new Employee { EmpId =1, EmpName="ABC", EmpSal=100}
+                                                        ,new Employee{ EmpId =2, EmpName ="XYZ", EmpSal=1000}
+                                                        , new Employee {EmpId = 3, EmpName="PQR",EmpSal=5000 } };
+
+            return oblist;
+        }
+    }
+}
