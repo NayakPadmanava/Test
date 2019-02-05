62aee510b7162a3e4d9a8b103c4bebfab3167bd3
Padmanava Nayak <nayak.padma84@gmail.com>
2019-02-05 09:14:27 +0530

    Strongly Typed view

---
+++ MVC/New folder/Controllers/HTMLHelperController.cs
@@ -0,0 +1,27 @@
+﻿using DemoModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoModel.Controllers
+{
+    public class HTMLHelperController : Controller
+    {
+        // GET: HTMLHelper
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DemoStronglyTyped()
+        {
+            Employee obEmp = new Employee();
+            obEmp.EmpId = "100";
+            obEmp.EmpName = "Srujana";
+            obEmp.EmpSal = 24000;
+            
+            return View(obEmp);
+        }
+    }
+}
